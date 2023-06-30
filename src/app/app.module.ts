0165ef9aaa40748c8114caa9d77dd15b3feac4c8
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-06-30 18:49:37 +0300

    add providers in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlertComponent } from './_components/alert/alert.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ErrorInterceptor, JwtInterceptor, appInitializer, fakeBackendProvider } from './_helpers';
+import { AccountService } from './_services';
 
 @NgModule({
   declarations: [
@@ -16,13 +18,20 @@ import { HomeComponent } from './home/home.component';
     AlertComponent,
     HomeComponent
   ],
-  imports: [ 
+  imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule, 
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [AccountService] },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
