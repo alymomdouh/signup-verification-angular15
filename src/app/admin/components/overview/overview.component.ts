9b382481b3803a65a30d5e0e4c1dc32b418d59a1
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-07-01 11:00:19 +0300

    add OnInit

--- src/app/admin/components/overview/overview.component.ts
+++ src/app/admin/components/overview/overview.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css']
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
+  ngOnInit(): void {
+    debugger
+  }
 
 }
